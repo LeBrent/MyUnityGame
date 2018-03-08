c76eb6c0f3bf060eb97d680c8e6791c0d778cb59
Brent Schoenmakers <kofenoen@outlook.com>
2018-03-08 21:35:35 +0100

    Collect 1 gold
    Collect one gold button now updating the text box

---
+++ Testing/Assets/Resources/Resource.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Resource : MonoBehaviour {
+    public int Quantity;
+    public UnityEvent OnQuantityChange = new UnityEvent();
+	// Use this for initialization
+
+    public void Add( int value )
+    {
+        Quantity += value;
+        OnQuantityChange.Invoke();
+    }
+}
---
+++ Testing/Assets/Resources/ResourceUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceUI : MonoBehaviour {
+    public Resource resource;
+    public Text text;
+    public void Start()
+    {
+      
+    }
+    public  void UpdateUI()
+    {
+        text.text = resource.Quantity.ToString();
+    }
+}
