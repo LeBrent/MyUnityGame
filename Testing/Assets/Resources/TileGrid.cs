00ed1feaccd91fdfe681b142b4a60abb9536366d
Brent Schoenmakers <kofenoen@outlook.com>
2018-03-09 14:10:39 +0100

    Grid
    A grid can be generated.No units on grid yet

---
+++ Testing/Assets/Resources/TileGrid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGrid : MonoBehaviour {
+    public int SizeX;
+    public int SizeY;
+   
+
+    public GridTile Tile;
+
+    public void Start()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        for (int i = 0; i < SizeX; i++)
+        {
+            for (int j = 0; j < SizeY; j++)
+            {
+                GridTile newTile = GameObject.Instantiate(Tile);
+                newTile.X = i;
+                newTile.Y = j;
+                newTile.name = "Tile (" + i.ToString() + ", " + j.ToString() + ")";
+                newTile.MoveTile();
+            }
+        }
+    }
+}
