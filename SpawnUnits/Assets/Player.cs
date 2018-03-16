c8c56231c8b6e7c604eead307454f5d25f2d3b17
Brent Schoenmakers <kofenoen@outlook.com>
2018-03-16 08:30:57 +0100

    PlayerOwned
    PlayerOwned has been made, although not very good, because the references will not know the owner, but the instances do.

---
+++ SpawnUnits/Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Color color;
+
+	// Use this for initialization
+    public void Disable()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void Enable()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
