174edd88b57fe83971d79bd8e0fc0f90bb66bdbb
Brent Schoenmakers <kofenoen@outlook.com>
2018-03-22 09:24:36 +0100

    Navmesh

--- SpawnUnits/Assets/GridModule/Unit.cs
+++ SpawnUnits/Assets/GridModule/Unit.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class Unit : MonoBehaviour 
 {
     public Player Owner;
+
+
 }
--- SpawnUnits/Assets/ResourceModule/PlayerOwned.cs
+++ SpawnUnits/Assets/ResourceModule/PlayerOwned.cs
@@ -2,7 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerOwned : MonoBehaviour {
+public class PlayerOwned : MonoBehaviour
+{
+    public Player owner;
+
+    public void Initialize(Player Owner)
+    {
+        Owner = owner;
+        Renderer rend = GetComponentInChildren<Renderer>();
+        if (rend != null)
+        {
+            rend.material.color = owner.color;
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
